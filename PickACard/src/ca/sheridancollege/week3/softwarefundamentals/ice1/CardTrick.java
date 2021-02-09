650c30243e22cad207ef63fe3c5e9d719657001c
kris-academic <rykk@sheridancollege.com>
2021-02-09 11:59:13 -0500

    removed Scanner import
    Krista Ryk
    99146369
    Feb 8 2021

--- PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ PickACard/src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,9 +5,6 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
-
-import java.util.Scanner;
-
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then
  * asks the user to pick a card and searches the array of cards for the match to
